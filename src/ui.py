caf0ec61aa2c94c6f8a8509c2dda3cc2914f9d7a
masukawa <spiral.spirit.spider@gmail.com>
2023-08-17 22:30:00 +0900

    create send to reader menu

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,6 @@
 from calibre.gui2.actions import InterfaceAction
 from calibre_plugins.readwise.main import ReadwiseDialog
+from qt.core import QMenu 
 
 class InterfacePlugin(InterfaceAction):
   name = 'Readwise'
@@ -9,6 +10,18 @@ class InterfacePlugin(InterfaceAction):
     icon = get_icons('images/icon.png')
     self.qaction.setIcon(icon)
     self.qaction.triggered.connect(self.show_dialog)
+    self.menu = QMenu(self.gui)
+    self.qaction.setMenu(self.menu)
+
+
+  def initialization_complete(self):
+      print(self.menu.__dict__)
+      self.create_menu_action(
+        self.menu, "send_to_reader", "Send to reader", 
+        icon=get_icons('images/upload_book.png'),
+        triggered=self.send_to_reader
+      )
+
 
   def show_dialog(self):
     base_plugin_object = self.interface_action_base_plugin
@@ -19,3 +32,7 @@ class InterfacePlugin(InterfaceAction):
   def apply_settings(self):
     from calibre_plugins.readwise.config import prefs
     prefs
+
+
+  def send_to_reader(self):
+    pass
